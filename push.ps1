2d738eeffe0193dd4c83ca83717012f228002b18
huhuiyu <1069306849@qq.com>
2023-09-04 09:06:09 +0800

    多仓库推送脚本

---
+++ push.ps1
@@ -0,0 +1,7 @@
+# git@github.com:huhuiyu/env.git
+# git remote add jihulab git@jihulab.com:DarkKnight/env.git
+# git remote add gitee git@gitee.com:huhuiyu/env.git
+git pull
+git push
+git push jihulab
+git push gitee
