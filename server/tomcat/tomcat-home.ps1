6a5203fc5a796e9157f0aceafe3813038bb3009a
DarkKnight <1069306849@qq.com>
2024-02-06 16:10:45 +0800

    tomcat路径修改

--- server/tomcat/tomcat-home.ps1
+++ server/tomcat/tomcat-home.ps1
@@ -1,3 +1,3 @@
 # 修改环境变量添加tomcat执行文件路径
-$env:TOMCAT8_HOME="D:\develop\server\apache-tomcat-8"
-$env:TOMCAT10_HOME="D:\develop\server\apache-tomcat-10"
+$env:TOMCAT8_HOME="D:\develop\env\server\apache-tomcat-8"
+$env:TOMCAT10_HOME="D:\develop\env\server\apache-tomcat-10"
