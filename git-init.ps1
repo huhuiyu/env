d47477484dbea31ba4d85b91a735bb41204e3958
huhuiyu <1069306849@qq.com>
2024-07-04 08:42:50 +0800

    修改稿主仓库为GitHub

--- git-init.ps1
+++ git-init.ps1
@@ -1,7 +1,5 @@
-# github地址：git@github.com:huhuiyu/env.git
-# 移除gitee和jihulab的仓库地址
-# git remote remove gitee
-git remote remove jihulab
-# 添加gitee和jihulab的仓库地址
-# git remote add gitee git@gitee.com:huhuiyu/env.git
-git remote add jihulab git@jihulab.com:DarkKnight/env.git
+# github仓库地址：git@github.com:huhuiyu/env.git
+# 移除gitee仓库地址
+git remote remove gitee
+# 添加gitee仓库地址
+git remote add gitee git@gitee.com:huhuiyu/env.git
--- push.ps1
+++ push.ps1
@@ -1,9 +1,7 @@
 # 多仓库推送
 git pull
 Write-Output "===========pull finish==========="
-git push jihulab
-Write-Output "===========push jihulab finish==========="
 git push
+Write-Output "===========push github finish==========="
+git push gitee
 Write-Output "===========push gitee finish==========="
-# git push
-# Write-Output "===========推送github完成==========="
